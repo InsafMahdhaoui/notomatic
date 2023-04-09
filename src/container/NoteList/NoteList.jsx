5c82f739e0e013590979177f0da0314427069193
InsafMahdhaoui <65137081+InsafMahdhaoui@users.noreply.github.com>
2023-04-09 11:05:53 +0100

    Update NoteList.jsx

--- src/container/NoteList/NoteList.jsx
+++ src/container/NoteList/NoteList.jsx
@@ -21,7 +21,7 @@ export function NoteList({ noteList }) {
           <div key={note.id} className={s.card_container}>
             <TextCard
               title={note.title}
-              subtitle={note.subtitle}
+              subtitle={note.created_at}
               content={note.content}
               onClick={() => navigate("/note/" + note.id)}
               onClickTrash={() => deletNote_(note)}
